5e6cec20864136fb07785da04c17c9b0345ea5d6
jofftiquez@gmail.com <jofftiquez@gmail.com>
2017-06-30 12:20:06 +0800

    - removed mixin - added component

---
+++ VueCroppie.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <div ref=croppieContainer id="croppieContainer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+  
+</style>
--- index.js
+++ index.js
@@ -2,13 +2,45 @@ import Croppie from 'croppie'
 import 'croppie/croppie.css'
 
 const VueCroppie = {
-  install(Vue, options) {
-  	Vue.mixin({
-      mounted() {
-        console.log('Mounted!');
-      }
-    });
-  }
+    install(Vue, options) {
+        const comp = {
+            render(h){
+                return h('div', {ref: 'croppieContainer', id: 'croppieContainer'})
+            },
+            mounted() {
+                this.initCroppie();
+            },
+            props: {
+                cropSrc: null,
+                cropped: null,
+                croppie: null,
+            },
+            methods: {
+                initCroppie() {
+                    let el = this.$refs.croppieContainer;
+                    this.croppie = new Croppie(el, {
+                        enableOrientation: true,
+                        viewport: {
+                            width: 250,
+                            height: 250,
+                        },
+                        boundary: {
+                            width: this.cropperWidth,
+                            height: 400
+                        },
+                        showZoomer: false,
+                    })
+                    this.croppie.bind(this.cropSrc)
+                },
+                crop() {
+                    this.croppie.result('base64').then(output => {
+                        this.$emit('on_crop', output)
+                    })
+                }
+            }
+        }
+        Vue.component('vue-coppie', comp)
+    }
 };
 
 export default VueCroppie;
