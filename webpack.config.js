6a17eb351671b299e367313524f2145ef65c4ab8
jofftiquez@gmail.com <jofftiquez@gmail.com>
2017-08-17 00:27:37 +0800

    Added var build

--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,65 @@ const path = require('path');
 const webpack = require('webpack');
 var CompressionPlugin = require('compression-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        library: 'VueCroppie',
-        libraryTarget: 'commonjs2',
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                loader:'style-loader!css-loader'
-            },
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['env']
+function createConfig(target) {
+    var name = 'index.js';
+    if(target === 'var') {
+        name = 'vue-croppie.js';
+    }
+    return {
+        name: target,
+        entry: './src/index.js',
+        output: {
+            library: 'VueCroppie',
+            libraryTarget: target,
+            path: path.resolve(__dirname, 'dist'),
+            filename: name
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    loader:'style-loader!css-loader'
+                },
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env']
+                        }
                     }
                 }
-            }
+            ]
+        },
+        plugins:[
+            new webpack.optimize.UglifyJsPlugin({
+                mangle: true,
+                compress: {
+                    warnings: false, // Suppress uglification warnings
+                    pure_getters: true,
+                    unsafe: true,
+                    unsafe_comps: true,
+                    screw_ie8: true
+                },
+                output: {
+                    comments: false,
+                },
+                exclude: [/\.min\.js$/gi] // skip pre-minified libs
+            }),
+            new CompressionPlugin({
+                asset: "[path].gz[query]",
+                algorithm: "gzip",
+                test: /\.js$|\.css$|\.html$/,
+                threshold: 10240,
+                minRatio: 0
+            })
         ]
-    },
-    plugins:[
-        new webpack.optimize.UglifyJsPlugin({
-            mangle: true,
-            compress: {
-                warnings: false, // Suppress uglification warnings
-                pure_getters: true,
-                unsafe: true,
-                unsafe_comps: true,
-                screw_ie8: true
-            },
-            output: {
-                comments: false,
-            },
-            exclude: [/\.min\.js$/gi] // skip pre-minified libs
-        }),
-        new CompressionPlugin({
-            asset: "[path].gz[query]",
-            algorithm: "gzip",
-            test: /\.js$|\.css$|\.html$/,
-            threshold: 10240,
-            minRatio: 0
-        })
-    ]
+    }
 }
+
+module.exports = [
+    createConfig('var'),
+    createConfig('commonjs2')
+]
