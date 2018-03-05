7483450d0c4f3ea60716e8bec11b299bb17f1e01
Joff Tiquez <jofftiquez@gmail.com>
2018-03-05 14:49:16 +0800

    Reindent

--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,53 @@ const path = require('path');
 const webpack = require('webpack');
 
 function createConfig(target) {
-    var name = 'index.js';
+  var name = 'index.js';
 
-    if(target !== 'commonjs2') {
-        name = 'vue-croppie.js'
-    }
+  if (target !== 'commonjs2') {
+    name = 'vue-croppie.js'
+  }
 
-    var output = {
-        library: 'VueCroppie',
-        libraryTarget: target,
-        path: path.resolve(__dirname, 'dist'),
-        filename: name
+  var output = {
+    library: 'VueCroppie',
+    libraryTarget: target,
+    path: path.resolve(__dirname, 'dist'),
+    filename: name
+  }
+
+  if (typeof target === 'undefined') {
+    name = 'vue-croppie.js';
+    output = {
+      path: path.resolve(__dirname, 'dist'),
+      filename: name
     }
-    
-    if(typeof target === 'undefined') {
-        name = 'vue-croppie.js';
-        output = {
-            path: path.resolve(__dirname, 'dist'),
-            filename: name
+  }
+
+  return {
+    name: target,
+    entry: './src/index.js',
+    output: output,
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader'
+        },
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['env']
+            }
+          }
         }
+      ]
     }
-
-    return {
-        name: target,
-        entry: './src/index.js',
-        output: output,
-        module: {
-            rules: [
-                {
-                    test: /\.css$/,
-                    loader:'style-loader!css-loader'
-                },
-                {
-                    test: /\.js$/,
-                    exclude: /(node_modules)/,
-                    use: {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['env']
-                        }
-                    }
-                }
-            ]
-        }
-    }
+  }
 }
 
 module.exports = [
-    createConfig('var'),
-    createConfig('commonjs2')
-]
+  createConfig('var'),
+  createConfig('commonjs2')
+];
